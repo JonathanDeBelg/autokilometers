2adf13837de0dcaeaf1a9008a4eced8bc43725c4
root <root@h2842416.stratoserver.net>
2020-08-18 20:13:33 +0200

    server changes

--- database/seeds/KilometerSeeder.php
+++ database/seeds/KilometerSeeder.php
@@ -13,10 +13,58 @@ class KilometerSeeder extends Seeder
     public function run()
     {
         DB::table('kilometers')->insert([
-            'mileage_old' => 0000000,
-            'mileage_new' => 332000,
+            'mileage_old' => 233041,
+            'mileage_new' => 233112,
             'by' => 'root',
             'costs_for_parents' => true,
+        ]);
+
+        DB::table('kilometers')->insert([
+            'created_at' => '2020-07-01 19:17:26',
+            'mileage_old' => 233041,
+            'mileage_new' => 233077,
+            'by' => 'laura',
+            'costs_for_parents' => false,
+        ]);
+
+        DB::table('kilometers')->insert([
+            'created_at' => '2020-07-02 19:17:26',
+            'mileage_old' => 233077,
+            'mileage_new' => 233112,
+            'by' => 'laura',
+            'costs_for_parents' => false,
+        ]);
+
+        DB::table('kilometers')->insert([
+            'created_at' => '2020-07-02 19:17:26',
+            'mileage_old' => 233112,
+            'mileage_new' => 233132,
+            'by' => 'nicolas',
+            'costs_for_parents' => false,
+        ]);
+
+        DB::table('kilometers')->insert([
+            'created_at' => '2020-07-03 19:17:26',
+            'mileage_old' => 233132,
+            'mileage_new' => 233170,
+            'by' => 'laura',
+            'costs_for_parents' => false,
+        ]);
+
+        DB::table('kilometers')->insert([
+            'created_at' => '2020-07-03 19:17:29',
+            'mileage_old' => 233170,
+            'mileage_new' => 233233,
+            'by' => 'laura',
+            'costs_for_parents' => true,
+        ]);
+            
+        DB::table('kilometers')->insert([
+                'created_at' => '2020-07-03 23:30:29',
+                'mileage_old' => 233233,
+                'mileage_new' => 233249,
+                'by' => 'jonathan',
+                'costs_for_parents' => false,
         ]);
     }
 }
